9b8c803f20fbe687b44fd9323918463e43baec26
Peeter Normak <peeter.normak@eesti.ee>
2019-09-30 21:33:47 +0300

    added OLED shield support

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,17 @@
 #include <webserver.hpp>
 #include <websocket.hpp>
 
+#ifdef ENABLE_OLED_SHIELD
+#include <screen.hpp>
+#endif
+
 WebServer * webserver = new WebServer();
 WebSocket * ws = new WebSocket(webserver);
 GWHomie * homie = new GWHomie();
 Com * comm = new Com(homie, ws);
+#ifdef ENABLE_OLED_SHIELD
+Screen * screen = new Screen();
+#endif
 
 #elif DEV_MODE == 2
 #include <now.hpp>
@@ -28,6 +35,9 @@ void setup() {
     homie->setup();
     webserver->setup();
     ws->setup();
+#ifdef ENABLE_OLED_SHIELD
+    screen->setup();
+#endif
 #elif DEV_MODE == 2
     now->setup();
 #endif
@@ -40,6 +50,9 @@ void loop() {
 #if DEV_MODE == 1
     homie->loop();
     ws->loop();
+#ifdef ENABLE_OLED_SHIELD
+    screen->loop();
+#endif
 #elif DEV_MODE == 2
     now->loop();
 #endif
